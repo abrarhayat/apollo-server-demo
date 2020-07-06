87e4048abf3e50428a4185034751f3871af7cfe0
Abrar Hayat <abrarhayat@gmail.com>
2020-07-07 00:26:58 +0600

    Implemented get queries for user and post types

--- index.js
+++ index.js
@@ -15,22 +15,35 @@ const typeDefs = gql`
     }
 
     type Query {
-        allUsers: [User!]!,
+        allUsers: [User!]!
         allPosts: [Post!]!
+        user(id : ID, name : String): User
+        post(id : ID, title : String): Post
     }
 `;
 
-users = [{name: 'Abrar', age : 26, id : '##$#$%REFDDFDSF'}, {name: 'Test', age : 30, id : '##$#$%REFDDFDSdfdfF'}]
-posts = [{title: 'Test Post', content: 'Test Post Description', id : '#$%$%$%$dse'}]
+users = [{ name: 'Abrar Hayat', age: 26, id: '##$#$%REFDDFDSF' }, { name: 'Christopher Williams', age: 30, id: '##$#$%REFDDFDSdfdfF' }, 
+{ name: 'Henry Burger', age: 35, id: '%$%$FDSGFGDFG' }]
+posts = [{ title: 'QraphQL', content: 'Demo for graphql', id: '#$%$%$%$dse'}, { title: 'Apollo Server', content: 'Demo for Apollo Server', id: '#$%$%$4545%$dse'}]
+
+function getUser(id, name) {
+    return users.find(user => user.id === id || user.name === name);
+}
+
+function getPost(id, title) {
+    return posts.find(post => post.id === id || post.title === title)
+}
 
 const resolvers = {
-    Query : {
-        allUsers : () => users,
-        allPosts : () => posts
+    Query: {
+        allUsers: () => users,
+        allPosts: () => posts,
+        user: (root, args, context, info) => getUser(args.id, args.name),
+        post: (root, args, context, info) => getPost(args.id, args.title)
     }
 }
 
-const server = new ApolloServer({typeDefs, resolvers})
+const server = new ApolloServer({ typeDefs, resolvers })
 
 server.listen().then(({ url }) => {
     console.log(`Server started at ${url}`)
