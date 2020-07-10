efe27452571685bfc31f7ddd37b03f699652f599
Abrar Hayat <abrarhayat@gmail.com>
2020-07-10 16:49:33 +0600

    Added new field to User type for nested object demo

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const typeDefs = gql`
         id: ID!
         name: String!
         age: Int!
+        skills: [Skill]
     }
 
     type Post {
         id: ID!
         title: String
         content: String
+    }
+
+    type Skill {
+        name: String
+        proficiency: Int
     }
 
     type Query {
@@ -22,9 +28,13 @@ const typeDefs = gql`
     }
 `;
 
-users = [{ name: 'Abrar Hayat', age: 26, id: '##$#$%REFDDFDSF' }, { name: 'Christopher Williams', age: 30, id: '##$#$%REFDDFDSdfdfF' }, 
-{ name: 'Henry Burger', age: 35, id: '%$%$FDSGFGDFG' }]
-posts = [{ title: 'QraphQL', content: 'Demo for graphql', id: '#$%$%$%$dse'}, { title: 'Apollo Server', content: 'Demo for Apollo Server', id: '#$%$%$4545%$dse'}]
+users = [{ name: 'Abrar Hayat', age: 26, id: '##$#$%REFDDFDSF', skills: [{ name: 'Java', proficiency: 10 }, { name: 'Python', proficiency: 8 }] }, {
+    name: 'Christopher Williams',
+    age: 30, id: '##$#$%REFDDFDSdfdfF', skills: [{ name: 'Kotlin', proficiency: 10 }, { name: 'Python', proficiency: 8 }]
+},
+{ name: 'Henry Burger', age: 35, id: '%$%$FDSGFGDFG', skills: [{ name: 'Kotlin', proficiency: 10 }, { name: 'Python', proficiency: 8 }] } ]
+
+posts = [{ title: 'QraphQL', content: 'Demo for graphql', id: '#$%$%$%$dse' }, { title: 'Apollo Server', content: 'Demo for Apollo Server', id: '#$%$%$4545%$dse' }]
 
 function getUser(id, name) {
     return users.find(user => user.id === id || user.name === name);
