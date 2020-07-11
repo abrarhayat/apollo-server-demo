cd27c122d93de0b481b6c655b1cfca95b03e48d4
Abrar Hayat <abrarhayat@gmail.com>
2020-07-12 03:15:30 +0600

    Implemented parameterized allUsers query and added some more sample queries

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const typeDefs = gql`
     }
 
     type Query {
-        allUsers: [User!]!
+        allUsers(olderThan: Int = -1): [User!]!
         allPosts: [Post!]!
         user(id : ID, name : String): User
         post(id : ID, title : String): Post
@@ -42,21 +42,31 @@ function getPost(id, title) {
     return posts.find(post => post.id === id || post.title === title)
 }
 
+function getAllUsersByAge(olderThan) {
+    allUsers = [];
+    users.forEach((user) => {
+        if (user.age > olderThan) {
+            allUsers.push(user);
+        }
+    })
+    return allUsers;
+}
+
 function getAllPostsFromUser(userId) {
     user = getUser(userId);
     currentUserPostIds = user.userPostsIds;
     allPostInfo = new Array();
     currentUserPostIds.forEach((post) => {
-        allPostInfo.push(getPost(post.id))
+        allPostInfo.push(getPost(post.id));
     })
     return allPostInfo;
 }
 
 const resolvers = {
     Query: {
-        allUsers: () => users,
+        allUsers: (root, args, context, info) => getAllUsersByAge(args.olderThan),
         allPosts: () => posts,
-        user: (root, args, context, info) => getUser(args.id, args.name),
+        user: (root, args, context, info) => getUser(args.id, args.name, args.olderThan),
         post: (root, args, context, info) => getPost(args.id, args.title)
     },
 
